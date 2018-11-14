a15be98bde0f16b831bda58ab8ddfb78cdd69e5f
Jamie <tino7060@likelion.org>
2018-11-14 21:36:14 +0900

    searchkick

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,3 +1,13 @@
 class Drug < ApplicationRecord
     has_and_belongs_to_many :dur_ingrs
+
+    searchkick word_middle: [:item_name, :ingr_kor_name, :ingr_eng_name]
+
+    def search_data
+        {
+            item_name: item_name,
+            ingr_kor_name: ingr_kor_name,
+            ingr_eng_name: ingr_eng_name
+        }
+    end
 end
--- app/models/supplement.rb
+++ app/models/supplement.rb
@@ -1,3 +1,13 @@
 class Supplement < ApplicationRecord
     has_and_belongs_to_many :supplement_ingrs
+    
+    searchkick word_middle: [:product_name, :ingredients]
+
+    def search_data
+        {
+            product_name: product_name,
+            ingredients: ingredients
+        }
+    end
+
 end
