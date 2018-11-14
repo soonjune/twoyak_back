453bedb8f8dbef5f445ff584af55f747d73ec25f
Jamie <tino7060@likelion.org>
2018-11-14 22:49:37 +0900

    connect to aws ES

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,9 +1,3 @@
-if Rails.env == "production"
-    url = "https://vpc-twoyak-fve2u4dzywn3fodzbfrryzxusi.ap-northeast-2.es.amazonaws.com"
-      Elasticsearch::Model.client = Elasticsearch::Client.new url: url
-    Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
-else
-    url = 'http://localhost:9200/'
-      Elasticsearch::Model.client = Elasticsearch::Client.new url: url
-    Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
-end
+url = "https://vpc-twoyak-fve2u4dzywn3fodzbfrryzxusi.ap-northeast-2.es.amazonaws.com"
+    Elasticsearch::Model.client = Elasticsearch::Client.new url: url
+  Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
