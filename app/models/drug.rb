28d84efae03568924dbd8067bff36f906ad33812
Jamie <tino7060@likelion.org>
2018-11-16 00:52:42 +0900

    test search_data

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,6 +1,20 @@
 class Drug < ApplicationRecord
     require 'json'
 
+    def my_map(enumerable)
+        result = []
+        enumerable.each { |element| result.push *(element) }
+        result
+      end
+      
+      def eng_map(enumerable)
+        result = []
+        enumerable.each do |element| 
+          element.ingr_eng_name.nil? ? next : result.push(*(JSON.parse(element.ingr_eng_name)))
+        end
+        result
+      end
+
     has_and_belongs_to_many :dur_ingrs
 
     searchkick word_middle: [:item_name, :ingr_kor_name, :ingr_eng_name]
@@ -8,7 +22,7 @@ class Drug < ApplicationRecord
     def search_data
         {
             name: item_name,
-            ingr_kor_name: JSON.parse(ingr_kor_name),
+            ingr_kor_name: eng_map(my_map(ingr_kor_name)),
             ingr_eng_name: JSON.parse(ingr_eng_name)
         }
     end
