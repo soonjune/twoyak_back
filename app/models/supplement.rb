db87bf71fa52aaf4e26055d7bafe8b437f0e16c8
Jamie <tino7060@likelion.org>
2018-11-19 22:30:59 +0900

    korean language

--- app/models/supplement.rb
+++ app/models/supplement.rb
@@ -2,7 +2,7 @@ class Supplement < ApplicationRecord
 
     has_and_belongs_to_many :supplement_ingrs
     
-    searchkick word_middle: [:product_name, :ingredients]
+    searchkick language: "korean", word_middle: [:product_name, :ingredients]
 
     def search_data
         {
