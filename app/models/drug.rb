30f35ae3581cc51104567549ef024ea8bb1a64af
Jamie <tino7060@likelion.org>
2018-11-20 12:42:36 +0900

    back to korean

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -20,7 +20,7 @@ class Drug < ApplicationRecord
 
     has_and_belongs_to_many :dur_ingrs
 
-    searchkick word_middle: [:item_name, :ingr_kor_name, :ingr_eng_name]
+    searchkick language: "korean", word_middle: [:item_name, :ingr_kor_name, :ingr_eng_name]
 
     def search_data
         {
