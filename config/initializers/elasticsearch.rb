fcdc77ab9140e5b9e4e91a31604b1147371478c6
Jamie <tino7060@likelion.org>
2018-11-27 16:00:11 +0900

    moving to twoyak

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,3 +1,3 @@
-url = "https://vpc-twoyak-fve2u4dzywn3fodzbfrryzxusi.ap-northeast-2.es.amazonaws.com"
+url = "https://vpc-twoyak-tgdpdr7k4moi4flysnhc4lsosu.ap-northeast-2.es.amazonaws.com"
     Elasticsearch::Model.client = Elasticsearch::Client.new url: url
   Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
--- config/initializers/elasticsearch_config.rb
+++ config/initializers/elasticsearch_config.rb
@@ -1,4 +1,4 @@
-ENV['ELASTICSEARCH_URL'] = "https://vpc-twoyak-fve2u4dzywn3fodzbfrryzxusi.ap-northeast-2.es.amazonaws.com"
+ENV['ELASTICSEARCH_URL'] = "https://vpc-twoyak-tgdpdr7k4moi4flysnhc4lsosu.ap-northeast-2.es.amazonaws.com"
 Searchkick.aws_credentials = {
   access_key_id: ENV['AWS_ACCESS_KEY_ID'],
   secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
