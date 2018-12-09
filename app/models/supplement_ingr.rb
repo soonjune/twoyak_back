f2e58511581f9f478f3394cc04753d6efea5759d
Jamie <tino7060@likelion.org>
2018-12-09 16:58:38 +0900

    model to search-supplement_ingrs

--- app/models/supplement_ingr.rb
+++ app/models/supplement_ingr.rb
@@ -1,3 +1,12 @@
 class SupplementIngr < ApplicationRecord
     has_and_belongs_to_many :supplements
+
+    searchkick language: "korean", word_middle: [:ingr_name]
+
+
+    def search_data
+        {
+            ingr_name: ingr_name
+        }
+    end
 end
