d6b13bf16c23049f48ba5b214b614b6573e46810
Jamie <tino7060@likelion.org>
2018-12-09 22:20:41 +0900

    go back

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -21,7 +21,7 @@ class Drug < ApplicationRecord
     has_and_belongs_to_many :dur_ingrs
     has_one :drug_imprint
 
-    searchkick language: "korean", synonyms: [["Aspirin", "acetylsalicylic acid"]], word_middle: [:item_name, :ingr_kor_name, :ingr_eng_name]
+    searchkick language: "korean", word_middle: [:item_name, :ingr_kor_name, :ingr_eng_name]
 
 
     def search_data
