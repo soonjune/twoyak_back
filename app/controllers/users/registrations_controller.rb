46f4c8fd59572e8ae95111c3cfbb70feec86191f
Jamie <tino7060@likelion.org>
2019-01-14 14:03:16 +0900

    errors specified for registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,19 +12,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   def create
     if User.new(user_params).valid?
-      nil
+      user = User.new(user_params)
     else
       render json: { errors: "이메일을 다시 한번 확인해주세요." }, status: :bad_request
     end
+    if UserInfo.new(info_params).user_name?
+      user.save
+      info = UserInfo.new(info_params)
+      info.user_id = user.id
+    else
+      render json: { errors: "필수 정보를 모두 채워주세요" }, status: :bad_request
+    end
     begin
-      if UserInfo.new(info_params).user_name?
-        user = User.new(user_params)
-        user.save
-        info = UserInfo.new(info_params)
-        info.user_id = user.id
-      else
-        render json: { errors: "필수 정보를 모두 채워주세요" }, status: :bad_request
-      end
       if info.save
         render json: { stauts: 'User created successfully', auth_token: JWT.encode(payload(user), ENV['SECRET_KEY_BASE'], 'HS256') }, status: :created
       end
