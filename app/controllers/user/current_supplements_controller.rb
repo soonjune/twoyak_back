1f967699dba5b44b7cf634cb33b3bf2dccddbba7
Jamie <tino7060@likelion.org>
2019-01-14 15:27:42 +0900

    drugs & supplement tracking CRUd

--- app/controllers/user/current_supplements_controller.rb
+++ app/controllers/user/current_supplements_controller.rb
@@ -1,6 +1,6 @@
 class User::CurrentSupplementsController < ApplicationController
   before_action :authenticate_request!
-  before_action :set_current_supplement, only: [:show, :destroy]
+  before_action :set_current_supplement, :search_term, only: [:create, :show, :destroy, :destroy_to_past]
 
   # GET /current_supplements
   def index
@@ -16,10 +16,8 @@ class User::CurrentSupplementsController < ApplicationController
 
   # POST /current_supplements
   def create
-    @current_supplement = Supplement.search(params[:sup_name], fields: [name: :exact])
-
-    if @current_supplement.save
-      render json: @current_supplement, status: :created, location: @current_supplement
+    if @current_supplement << Supplement.search(@search_term, fields: [name: :exact]) 
+      render json: @current_supplement, status: :created
     else
       render json: @current_supplement.errors, status: :unprocessable_entity
     end
@@ -36,13 +34,26 @@ class User::CurrentSupplementsController < ApplicationController
 
   # DELETE /current_supplements/1
   def destroy
-    @current_supplement.destroy
+    @current_supplement.delete(Supplement.search(@search_term, fields: [name: :exact]))
+  end
+
+  def destroy_to_past
+    @current_supplement.delete(Supplement.search(@search_term, fields: [name: :exact]))
+    @past_supplements =  UserInfo.find(params[:user_info_id]).past_sup
+    @past_supplements << Supplement.search(@search_term, fields: [name: :exact])
+    render json: @past_supplements
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_current_supplement
-      @current_supplement = UserInfo.find(params[:id]).current_supplement
+      if current_user.user_info_ids.include? params[:user_info_id].to_i
+        @current_supplement = UserInfo.find(params[:user_info_id]).current_sup
+      end
+    end
+
+    def search_term
+      @search_term = params[:search_term]
     end
 
 end
