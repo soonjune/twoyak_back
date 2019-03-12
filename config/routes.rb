9978e578c52d0cde6a63b7963bab59b2f2e2f004
Jamie <tino7060@likelion.org>
2019-03-12 11:42:15 +0900

    minor typo fix in routes suppllements

--- config/routes.rb
+++ config/routes.rb
@@ -53,7 +53,7 @@ Rails.application.routes.draw do
   resources :drugs, :except => [:index] do
     resources :drug_reviews
   end
-  resources :suppllements, :except => [:index] do
+  resources :supplements, :except => [:index] do
     resources :sup_reviews
   end
   # get "drugs/:search_term" => "drugs#show"
