f404f432e5f420095baf240d49819b8f0b2bdd3c
Jamie <tino7060@likelion.org>
2019-03-15 12:38:45 +0900

    add user_info_id to token

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -67,8 +67,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   return nil unless user and user.id
   {
               :iss => "twoyak.com",
-              :user => {id: user.id, email: user.email, user_name: user.user_infos.first.user_name },
-              :exp => Time.now.to_i + 604800, #1week from now
+              :user => {id: user.id, email: user.email, user_name: user.user_infos.first.user_name, user_info_id: user.user_infos.first.id },
   }
   end
 
