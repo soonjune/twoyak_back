ae5a67ffce62c1da14c7ea13cde2a584abbdd97a
Jamie <tino7060@likelion.org>
2019-03-26 21:26:41 +0900

    admin data fix

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,14 +3,27 @@ class AdminController < ApplicationController
     before_action :is_admin?
   
     def index
-      @photos = PrescriptionPhoto.all
+      @photos = PrescriptionPhoto.all.order("id DESC")
+      @current_drugs = CurrentDrug.all.order("created_at DESC").limit(100)
     end
   
     def insert
-      drug_to_insert = Drug.find(insert_params[:drug_id])
-      user_info_insert = UserInfo.find(insert_params[:user_info_id]).currents << drug_to_insert
+      if @current_drug.include?(insert_params[:drug_id])
+        render json: { errors: "이미 투약 중인 의약품입니다." }, status: :unprocessable_entity
+      elsif @current_drug << Drug.find(insert_params[:drug_id])
+        set_time_memo = CurrentDrug.where(user_info_id: params[:user_info_id], current_drug_id: @search_id).last
+        set_time_memo.update(from: params[:from], to: params[:to] ? params[:to] : Time.zone.now, memo: params[:memo], when: params[:whern], how: params[:how])
+        render json: @current_drug.pluck(:id, :name), status: :created
+      else
+        render json: @current_drug.errors, status: :unprocessable_entity
+      end
     end
-  
+    
+
+    def destroy
+        CurrentDrug.find(:id).delete
+    end
+
     private
   
     def insert_params
@@ -21,7 +34,7 @@ class AdminController < ApplicationController
       if current_user.has_role? "admin"
         return
       else
-        render json: { errors: ['리뷰 작성 권한이 없습니다.'] }, status: :unauthorized
+        render json: { errors: ['접속 권한이 없습니다.'] }, status: :unauthorized
       end
     end
   end
