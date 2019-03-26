e1ec893e8e7c641a5ba08286e84e2f06b2fe5e72
Jamie <tino7060@likelion.org>
2019-03-26 11:09:34 +0900

    delete user_id

--- app/controllers/user/user_infos_controller.rb
+++ app/controllers/user/user_infos_controller.rb
@@ -49,6 +49,6 @@ class User::UserInfosController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_info_params
-      params.permit(:user_id, :user_name, :profile_image, :birth_date, :drink, :smoke, :caffeine, :sex)
+      params.permit(:user_name, :profile_image, :birth_date, :drink, :smoke, :caffeine, :sex)
     end
 end
