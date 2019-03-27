07fd46aec2a23d464439bf7a1270c038d6d1f5a9
Jamie <tino7060@likelion.org>
2019-03-27 11:57:40 +0900

    fix admin for insert

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,7 @@
 class AdminController < ApplicationController
     before_action :authenticate_request!
     before_action :is_admin?
+    before_action :set_current_drug, only: [:insert]
   
     def index
       @result = Hash.new
@@ -18,8 +19,8 @@ class AdminController < ApplicationController
       if @current_drug.include?(Drug.find(insert_params[:drug_id]))
         render json: { errors: "이미 투약 중인 의약품입니다." }, status: :unprocessable_entity
       elsif @current_drug << Drug.find(insert_params[:drug_id])
-        set_time_memo = CurrentDrug.where(user_info_id: params[:user_info_id], current_drug_id: @search_id).last
-        set_time_memo.update(from: params[:from], to: params[:to] ? params[:to] : Time.zone.now, memo: params[:memo], when: params[:whern], how: params[:how])
+        set_time_memo = CurrentDrug.where(user_info_id: insert_params[:user_info_id], current_drug_id: insert_params[:drug_id]).last
+        set_time_memo.update(from: insert_params[:from], to: insert_params[:to] ? insert_params[:to] : Time.zone.now, memo: insert_params[:memo], when: insert_params[:whern], how: insert_params[:how])
         render json: @current_drug.pluck(:id, :name), status: :created
       else
         render json: @current_drug.errors, status: :unprocessable_entity
@@ -51,4 +52,18 @@ class AdminController < ApplicationController
         render json: { errors: ['접속 권한이 없습니다.'] }, status: :unauthorized
       end
     end
+
+    def set_current_drug
+        if current_user.has_role? "admin"
+          @current_drug = UserInfo.find(params[:user_info_id]).current_drug
+        else
+          if current_user.user_info_ids.include? params[:user_info_id].to_i
+            @current_drug = UserInfo.find(params[:user_info_id]).current_drug
+          else
+            render json: { errors: "잘못된 접근입니다." }, status: :bad_request
+            return
+          end
+        end
+      end
+
   end
