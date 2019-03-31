c32108ebb907dafb91a4040ca398ff84064a99cc
Jamie <tino7060@likelion.org>
2019-03-31 20:30:48 +0900

    delete byebug

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -50,9 +50,9 @@ class AdminController < ApplicationController
         begin
             uri = URI("http://54.180.189.64:8001/api/push/target")
             target = ["2ee76634-1d9c-48f0-becc-3620f9f9abe3"]
-            # user = User.find(push_params["user_id"])
-            # user_token = user.push_token
-            # target << user_token
+            user = User.find(push_params["user_id"])
+            user_token = user.push_token
+            target << user_token
             data = {message: push_params["message"], target: target.to_s}
 
             http = Net::HTTP.new(uri.host, uri.port)
@@ -60,7 +60,6 @@ class AdminController < ApplicationController
             req.set_form_data(data)
             
             response = http.request(req)
-            byebug
             render json: user, status: 200
         rescue
             render json: "푸시 알람 실패", status: 500
