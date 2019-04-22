e75aa10d46d4b8154168f298918123cd2f794221
Jamie <tino7060@likelion.org>
2019-04-22 14:03:33 +0900

    fix routes for review index

--- config/routes.rb
+++ config/routes.rb
@@ -68,6 +68,8 @@ Rails.application.routes.draw do
   #모바일 토큰 변환
   post "change" => "change_token#change"
 
+  #전체 리뷰
+  get "reviews" => "drug_reviews#all"
   #최근 리뷰 받기
   get "reviews/recent" => "drug_reviews#recent"
   resources :drugs, :except => [:index] do
