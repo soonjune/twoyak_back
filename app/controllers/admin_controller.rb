b3b258013865d779ac7bd017fa314368385ad16d
Jamie <tino7060@likelion.org>
2019-04-30 17:31:59 +0900

    test

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -19,23 +19,39 @@ class AdminController < ApplicationController
       end
     end
 
-    def drug_analysis
-      @result = Hash.new
-      @current_drugs = CurrentDrug.all
-      @current_drugs.map { |drug|
-        temp = drug.as_json
-        temp["drug_name"] = Drug.find(drug.current_drug_id).name
-        temp["user_info"] = UserInfo.find(drug.user_info_id)
-        @result["current_drug"] = temp
+    def user_analysis
+      @data_sent = []
+      user_infos = UserInfo.all
+      user_infos.map { |user_info|
+        @past_diseases = []
+        user_info.past_diseases.each { |d|
+          @past_diseases << { id: d.id, parent_id: d.past_disease.id, name: d.past_disease.name, from: d.from, to: d.to }
+        }
+        @current_diseases = []
+        user_info.current_diseases.each { |d|
+          @current_diseases << { id: d.id, parent_id: d.current_disease.id, name: d.current_disease.name, from: d.from, to: d.to }
+        }
+        user_info.past_drugs.each { |d|
+          @past_drugs << { id: d.id, parent_id: d.past_drug.id, name: d.past_drug.name, from: d.from, to: d.to, memo: d.memo }
+        }
+        user_info.current_drugs.each { |d|
+          @current_drugs << { id: d.id, parent_id: d.current_drug.id, name: d.current_drug.name, from: d.from, to: d.to, memo: d.memo  }
+        }
+        info_data = { user_info: { basic_info: user_info, family_med_his: user_info.med_his.select(:id, :name), past_diseases: @past_diseases, 
+          current_diseases: @current_diseases, 
+          past_drugs: @past_drugs, 
+          current_drugs: @current_drugs}
+        }
+          #각각의 데이터를 넣어줌
+          infos << info_data
+        @data_sent[:infos] = infos
+        @data_sent[:watch_drugs] = current_user.watch_drug
+        @data_sent[:watch_supplements] = current_user.watch_supplement
+        @data_sent[:drug_reviews] = current_user.drug_reviews
+        @data_sent[:sup_reviews] = current_user.sup_reviews
       }
-      @past_drugs = PastDrug.all
-      @past_drugs.map { |drug|
-        temp = drug.as_json
-        temp["drug_name"] = Drug.find(drug.past_drug_id).name
-        temp["user_info"] = UserInfo.find(drug.user_info_id)
-        @result["past_drug"] = temp
-      }
-      render json: @result
+
+      render json: @data_sent
 
     end
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   #관리자용
   get "admin" => "admin#index"
-  get "admin/drug_analysis" => "admin#drug_analysis"
+  get "admin/user_analysis" => "admin#user_analysis"
   post "admin/check" => "admin#check"
   post "admin/push" => "admin#push"
   post "admin/push_all" => "admin#push_all"
