25473e2a8034c4813117f9ccc5504cbc89d4c7f4
Jamie <tino7060@likelion.org>
2019-05-27 15:03:50 +0900

    add social login

--- vendor/bundle/ruby/2.5.0/gems/devise-4.5.0/test/rails_app/app/controllers/users/omniauth_callbacks_controller.rb
+++ vendor/bundle/ruby/2.5.0/gems/devise-4.5.0/test/rails_app/app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,16 +1,36 @@
-# frozen_string_literal: true
-
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    data = request.respond_to?(:get_header) ? request.get_header("omniauth.auth") : request.env["omniauth.auth"]
-    session["devise.facebook_data"] = data["extra"]["user_hash"]
-    render json: data
+  def self.provides_callback_for(provider)
+    class_eval %Q{
+      def #{provider}
+        @user = User.find_for_oauth(request.env["omniauth.auth"], current_user)
+        if (@user.class == User && @user.persisted?)
+          sign_in(@user, store: false)
+          render json: { auth_token: JWT.encode(payload(@user), ENV['SECRET_KEY_BASE'], 'HS256') }
+          return
+        elsif @user == "already exists"
+          render json: { errors: '이메일로 직접 가입하셨거나 다른 소셜 계정으로 가입한 이메일입니다.' }, status: :unauthorized
+        else
+          session["devise.#{provider}_data"] = request.env["omniauth.auth"]
+          redirect_to new_user_registration_url
+        end
+      end
+    }
   end
 
-  def sign_in_facebook
-    user = User.to_adapter.find_first(email: 'user@test.com')
-    user.remember_me = true
-    sign_in user
-    render (Devise::Test.rails5? ? :body : :text) => ""
+  [:facebook, :google_oauth2, :naver].each do |provider|
+    provides_callback_for provider
   end
+
+  def after_sign_in_path_for(resource)
+    return
+  end
+
+  def payload(user)
+    return nil unless user and user.id
+    {
+                :iss => "twoyak.com",
+                :user => {id: user.id, email: user.email, user_name: user.user_infos.first.user_name, user_info_id: user.user_infos.first.id },
+    }
+    end
+  
 end
