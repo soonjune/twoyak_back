131e6c65ad28143f6f720271887d20b9584d1da5
Jamie <tino7060@likelion.org>
2019-05-30 19:20:18 +0900

    routing for direct get_by_drug dur

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,9 @@ Rails.application.routes.draw do
   namespace :user do
     resources :mypage
     resources :user_infos, :except => [:index]
+    #의약품으로 직접 안전정보(DUR) 가져오기
+    get 'analysis/get_by_drug'
+
     scope ':user_info_id' do
       #DUR 정보
       get 'analysis/get'
