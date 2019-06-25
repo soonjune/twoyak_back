bacb785a48d9dff3f1e26b60d93b2c6d7e0b847b
Jamie <tino7060@likelion.org>
2019-06-25 20:30:14 +0900

    fix drug controller to render same page

--- app/controllers/drugs_controller.rb
+++ app/controllers/drugs_controller.rb
@@ -89,17 +89,21 @@ class DrugsController < ApplicationController
   
     searched.each { |item|
       if(item.class == Drug && search == item.name)
-        @rep = item
-        @data["drug_id"] = @rep.id
-        @data["ingr_kor_name"] = JSON.parse(item.ingr_kor_name).uniq.to_s
-        @data["ingr_eng_name"] = item.ingr_eng_name
-        @data["atc_code"] = item.atc_code
-        @data["taking"] = item.currents.count
-        @data["watching"] = item.watch_drugs.pluck(:user_id)
-        if(!item.drug_imprint.nil?)
-          @data["drug_imprint"] = item.drug_imprint
-        end
-        break
+        #drugs/:id로 직접 접근하는 것과 동일한 화면 표시를 위해 redirection
+        @id = item.id
+        redirect_to drug_path(@id)
+        #검색 이전 코드
+        # @rep = item
+        # @data["drug_id"] = @rep.id
+        # @data["ingr_kor_name"] = JSON.parse(item.ingr_kor_name).uniq.to_s
+        # @data["ingr_eng_name"] = item.ingr_eng_name
+        # @data["atc_code"] = item.atc_code
+        # @data["taking"] = item.currents.count
+        # @data["watching"] = item.watch_drugs.pluck(:user_id)
+        # if(!item.drug_imprint.nil?)
+        #   @data["drug_imprint"] = item.drug_imprint
+        # end
+        return
       elsif(item.class == Supplement && search == item.name)
         @sup = item
         break
