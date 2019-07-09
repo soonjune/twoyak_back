f7650d9c58760a97d755d9765cb522ceb01fed44
Jamie <tino7060@likelion.org>
2019-07-09 20:17:46 +0900

    fix pluck to select when showing adverse effect

--- lib/review_view.rb
+++ lib/review_view.rb
@@ -6,11 +6,15 @@ module ReviewView
         temp["id"] = review.id
         temp["drug_id"] = review.drug_id
         temp["efficacy"] = review.efficacy
-        temp["adverse_effects"] = review.adverse_effects.pluck(:id, :symptom_name)
+        temp["adverse_effects"] = review.adverse_effects.select(:id, :symptom_name)
         temp["body"] =review.body
         temp["liked_users"] = review.l_users.count
 
         return temp
+    end
+
+    def get_rate(drug)
+
     end
 
 end
