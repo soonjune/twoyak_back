c2fceef3c80d0fffb3a3ea95ca3652732336daac
Jamie <tino7060@likelion.org>
2019-07-10 20:23:51 +0900

    fix ominiauth callback for social login to render json

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,13 +3,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     class_eval %Q{
       def #{provider}
         @user = User.find_for_oauth(request.env["omniauth.auth"], current_user)
-        if @user.persisted?
+        if (@user.class == User && @user.persisted?)
           sign_in(@user, store: false)
-          render json: { auth_token: JWT.encode(payload(@user), ENV['SECRET_KEY_BASE'], 'HS256') }
-          return
+          @token = JWT.encode(payload(@user), ENV['SECRET_KEY_BASE'], 'HS256')
+          render json: { auth_token: @token }
         else
-          session["devise.#{provider}_data"] = request.env["omniauth.auth"]
-          redirect_to new_user_registration_url
+          render json: { errors: "소셜 로그인을 하실 수 없습니다. 같은 이메일로 가입하시지 않으셨나요?" }, status: :unauthorized
         end
       end
     }
@@ -27,8 +26,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     return nil unless user and user.id
     {
                 :iss => "twoyak.com",
-                :user => {id: user.id, email: user.email, user_name: user.user_infos.first.user_name, user_info_id: user.user_infos.first.id },
+                :user => {id: user.id, email: user.email, user_name: user.user_infos.first.user_name, user_info_id: user.user_infos.first.id, login_count: user.sign_in_count},
     }
-    end
+  end
+
+  def flash
+    super
+  end
   
 end
