27da91cc4fd33f90669b5a15e300e2dfade777f3
Jamie <tino7060@likelion.org>
2019-07-10 22:59:05 +0900

    fix user_info update encode jwt

--- app/controllers/change_token_controller.rb
+++ app/controllers/change_token_controller.rb
@@ -30,7 +30,7 @@ class ChangeTokenController < ApplicationController
         return nil unless user and user.id
         {
                     :iss => "twoyak.com",
-                    :user => {id: user.id, email: user.email, user_name: user.user_infos.first.user_name, user_info_id: user.user_infos.first.id },
+                    :user => {id: user.id, email: user.email, user_name: user.user_infos.first.user_name, user_info_ids: user.user_infos.pluck(:id) },
         }
         end
 end
