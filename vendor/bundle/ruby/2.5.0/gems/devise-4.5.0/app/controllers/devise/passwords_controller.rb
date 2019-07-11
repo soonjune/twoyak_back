8ec857e1953c8042d0052be8c53b96d8f26a2899
Jamie <tino7060@likelion.org>
2019-07-11 22:10:22 +0900

    fix email authentication and password resetting

--- vendor/bundle/ruby/2.5.0/gems/devise-4.5.0/app/controllers/devise/passwords_controller.rb
+++ vendor/bundle/ruby/2.5.0/gems/devise-4.5.0/app/controllers/devise/passwords_controller.rb
@@ -31,22 +31,12 @@ class Devise::PasswordsController < DeviseController
 
   # PUT /resource/password
   def update
-    self.resource = resource_class.reset_password_by_token(resource_params)
-    yield resource if block_given?
+    user = User.with_reset_password_token(params[:reset_password_token])
 
-    if resource.errors.empty?
-      resource.unlock_access! if unlockable?(resource)
-      if Devise.sign_in_after_reset_password
-        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
-        set_flash_message!(:notice, flash_message)
-        sign_in(resource_name, resource)
-      else
-        set_flash_message!(:notice, :updated_not_active)
-      end
-      respond_with resource, location: after_resetting_password_path_for(resource)
+    if user&.update(password_params)      
+      render json: user, status: :ok
     else
-      set_minimum_password_length
-      respond_with resource
+      render json: {error: ['Password update failed']}, status: :internal_server_error
     end
   end
 
@@ -78,5 +68,11 @@ class Devise::PasswordsController < DeviseController
 
     def translation_scope
       'devise.passwords'
+    end
+
+    private
+
+    def password_params
+      params.permit(:password)
     end
 end
