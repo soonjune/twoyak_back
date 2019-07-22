82bd5cc3f9c97bfa51624ff22e7debbbc184a6fd
Jamie <tino7060@likelion.org>
2019-07-22 22:22:21 +0900

    fix column adding to reference adding (difference in schema

--- db/migrate/20190722130858_add_drug_ingrs_referece_to_interactions.rb
+++ db/migrate/20190722130858_add_drug_ingrs_referece_to_interactions.rb
@@ -1,5 +1,5 @@
 class AddDrugIngrsRefereceToInteractions < ActiveRecord::Migration[5.2]
   def change
-    add_column :interactions, :drug_ingr_id, :bigint, after: 'id', index: true
+    add_reference :interactions, :drug_ingr, after: 'id', index: true
   end
 end
