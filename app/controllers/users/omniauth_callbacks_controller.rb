52e07def3524c736496b497a2c83ba3a62efcd3c
Jamie <tino7060@likelion.org>
2019-07-31 21:59:08 +0900

    fix redirect for omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,14 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def self.provides_callback_for(provider)
+    require 'payload'
     class_eval %Q{
       def #{provider}
         @user = User.find_for_oauth(request.env["omniauth.auth"], current_user)
         if (@user.class == User && @user.persisted?)
           sign_in(@user, store: false)
-          @token = JWT.encode(payload_for_social(@user), ENV['SECRET_KEY_BASE'], 'HS256')
-          render json: { auth_token: @token }
+          uri = URI("http://localhost:3000/login")
+          redirect_to uri.to_s, Payload.jwt_encoded(@user)
         else
-          render json: { errors: "소셜 로그인을 하실 수 없습니다. 같은 이메일로 가입하시지 않으셨나요?" }, status: :unauthorized
+          uri = URI("http://localhost:3000/login-error")
+          redirect_to uri.to_s
         end
       end
     }
@@ -22,11 +24,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     return
   end
 
-  def payload_for_social(user)
+  def payload(user)
     return nil unless user and user.id
     {
                 :iss => "twoyak.com",
-                :user => {id: user.id, email: user.email, sub_users: user.sub_users.select(:id, :user_name).as_json, login_count: user.sign_in_count},
+                :user => {id: user.id, email: user.email, sub_users: user.sub_users.select(:id, :user_name).as_json },
     }
   end
 
