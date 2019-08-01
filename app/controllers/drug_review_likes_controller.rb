89507dcc9b47737b84d476215ec7be1404581b7b
Jamie <tino7060@likelion.org>
2019-08-02 02:54:05 +0900

    trailing 4

--- app/controllers/drug_review_likes_controller.rb
+++ app/controllers/drug_review_likes_controller.rb
@@ -3,11 +3,7 @@ class DrugReviewLikesController < ApplicationController
 
     #좋아요 눌렀는지 확인
     def show
-<<<<<<< HEAD
         if current_user.l_drug_reviews.include?(params[:review_id])
-=======
-        if current_user.l_drug_reviews.include?(params[:id])
->>>>>>> master
             result = true
         else
             result = false
@@ -16,7 +12,6 @@ class DrugReviewLikesController < ApplicationController
     end
 
     def like_toggle
-<<<<<<< HEAD
         like = DrugReviewLike.find_by(user: current_user, drug_review_id: params[:drug_review_id])
         if like.nil?
             DrugReviewLike.create(user: current_user, drug_review_id: params[:drug_review_id])
@@ -27,14 +22,4 @@ class DrugReviewLikesController < ApplicationController
         end
     end
 
-=======
-        like = Like.find_by.find_by(user: current_user, drug_review_comment_id: params[:id])
-        if like.nil?
-            DrugReviewLike.create(user: current_user, drug_review_comment_id: params[:id])
-        else
-            like.destroy
-        end
-        render json: {status: 'ok'}, status: :ok
-    end
->>>>>>> master
 end
