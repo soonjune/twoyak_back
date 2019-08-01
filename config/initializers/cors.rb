2e0b88bfb7fab45dd8fddb8e4cbe663c3d8da6f8
Jamie <tino7060@likelion.org>
2019-08-02 05:19:06 +0900

    fix https://twoyak.com cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
 allow do
-  origins '*'
+  origins 'https://twoyak.com'
   resource '*', headers: :any, methods: :get
 
   # Only allow a request for a specific host
