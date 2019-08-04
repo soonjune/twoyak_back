b6f0b05110f62ccc497db6f1d25c3350d782bc25
Jamie <tino7060@likelion.org>
2019-08-04 22:12:34 +0900

    revert elastic config

--- config/initializers/elasticsearch_config.rb
+++ config/initializers/elasticsearch_config.rb
@@ -1,10 +1,6 @@
-require 'faraday_middleware/aws_signers_v4'
-
 ENV['ELASTICSEARCH_URL'] = "https://vpc-twoyak-tgdpdr7k4moi4flysnhc4lsosu.ap-northeast-2.es.amazonaws.com"
-
-Searchkick.client = Elasticsearch::Client.new(url: ENV['ELASTICSEARCH_URL']) do |f|
-  f.request :aws_signers_v4,
-            credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
-            service_name: 'es',
-            region: ENV['AWS_REGION']
-end
+Searchkick.aws_credentials = {
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  region: ENV['AWS_REGION']
+}
