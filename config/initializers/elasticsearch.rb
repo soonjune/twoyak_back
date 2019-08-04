42ac7dc2be335488aceeb4c52237b6012092089f
Jamie <tino7060@likelion.org>
2019-08-04 21:48:31 +0900

    rever elasticsearch change

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,22 +1,3 @@
-require "faraday_middleware/aws_signers_v4"
-
-class AmazonElasticSearchClient
-  def self.client
-    return Elasticsearch::Client.new(url: "https://vpc-twoyak-tgdpdr7k4moi4flysnhc4lsosu.ap-northeast-2.es.amazonaws.com") do |f|
-      f.request :aws_signers_v4,
-                service_name: 'es',
-                credentials: Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"]),
-                region: ENV["AWS_REGION"],
-                # This is standard for Net::HTTP, if you
-                # you are using another client with Faraday,
-                # it might be configured using other keys.
-                timeout: 20,
-                open_timeout: 10
-    end
-  end
-end
-
-# We use AWS_ELASTICSEARCH_URL instead of ELASTICSEARCH_URL
-# as we alsow want to support environments that are not
-# connecting to a AWS ES endpoint.
-Elasticsearch::Model.client = AmazonElasticSearchClient.client unless ENV["AWS_ELASTICSEARCH_URL"].blank?
+url = "https://vpc-twoyak-tgdpdr7k4moi4flysnhc4lsosu.ap-northeast-2.es.amazonaws.com"
+    Elasticsearch::Model.client = Elasticsearch::Client.new url: url
+  Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
