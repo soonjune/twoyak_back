9773dd151bf94521effead54bfc106cd6d402c86
Jamie <tino7060@likelion.org>
2019-08-04 22:07:34 +0900

    fix connection

--- config/initializers/elasticsearch_config.rb
+++ config/initializers/elasticsearch_config.rb
@@ -1,8 +1,10 @@
 require 'faraday_middleware/aws_signers_v4'
 
-client = Elasticsearch::Client.new url: ENV['ELASTICSEARCH_URL'] do |f|
+ENV['ELASTICSEARCH_URL'] = "https://vpc-twoyak-tgdpdr7k4moi4flysnhc4lsosu.ap-northeast-2.es.amazonaws.com"
+
+Searchkick.client = Elasticsearch::Client.new(url: ENV['ELASTICSEARCH_URL']) do |f|
   f.request :aws_signers_v4,
-            credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY'], ENV['AWS_SECRET_ACCESS_KEY']),
+            credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
             service_name: 'es',
             region: ENV['AWS_REGION']
 end
