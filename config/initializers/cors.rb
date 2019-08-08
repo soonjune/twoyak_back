26cd4ee99c8ad70cde7729ddd51fd7ef0acff1ac
Jamie <tino7060@likelion.org>
2019-08-08 14:32:00 +0900

    add admin page to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'localhost:3000', '127.0.0.1:3000', 'xenodochial-leakey-4d32bd.netlify.com',
             #허재혁 netlify
             'unruffled-minsky-753432.netlify.com',
+            #관리자 페이지
+            'mvponstreet.s3-website.ap-northeast-2.amazonaws.com',
             /\Ahttp:\/\/163\.152\.83\.\168(:\d+)?\z/
 
     resource '*',
