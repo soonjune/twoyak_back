17a50c3e0de1e0f2dd15c3a59daa00ab4f206b4b
Jamie <tino7060@likelion.org>
2019-08-09 15:36:33 +0900

    update password reset

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,18 +1,69 @@
 class Users::PasswordsController < Devise::PasswordsController
     def create
-        self.resource = resource_class.send_reset_password_instructions(user_params)
+        self.resource = resource_class.send_reset_password_instructions(params)
+    
         if successfully_sent?(resource)
-        render json: {status: 'ok'}, status: :ok
+          render json: {status: 'ok'}, status: :ok
         else
-        render json: {error: ['Error occurred']}, status: :internal_server_error
+          render json: {error: ['Error occurred']}, status: :internal_server_error
         end
     end
-        
-
-    private
-
-    def user_params
-        params.permit(:email, :password, :password_confirmation)
+    
+      # GET /user/password/edit?reset_password_token=blablabla
+      def edit
+        self.resource = resource_class.new
+        set_minimum_password_length
+        resource.reset_password_token = params[:reset_password_token]
+        uri = URI("https://twoyak.com/reset-password")
+        uri.query = URI.encode_www_form(:token => params[:reset_password_token])
+        redirect_to uri.to_s
+      end
+    
+      # PUT /user/password
+      def update
+        self.resource = resource_class.reset_password_by_token(resource_params)
+        yield resource if block_given?
+    
+        if resource.errors.empty?
+          resource.unlock_access! if unlockable?(resource)
+          if Devise.sign_in_after_reset_password
+            flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+            set_flash_message!(:notice, flash_message)
+            resource.after_database_authentication
+            sign_in(resource_name, resource)
+          else
+            set_flash_message!(:notice, :updated_not_active)
+          end
+          render json: {
+            success: true,
+            data: resource.as_json,
+            message: I18n.t("devise_token_auth.passwords.successfully_updated")
+          }
+        else
+          set_minimum_password_length
+          respond_with resource
+        end
     end
-
+    
+    
+      protected
+        def after_resetting_password_path_for(resource)
+          Devise.sign_in_after_reset_password ? after_sign_in_path_for(resource) : new_session_path(resource_name)
+        end
+    
+        # Check if a reset_password_token is provided in the request
+        def assert_reset_token_passed
+          if params[:reset_password_token].blank?
+            set_flash_message(:alert, :no_token)
+            redirect_to new_session_path(resource_name)
+          end
+        end
+    
+        # Check if proper Lockable module methods are present & unlock strategy
+        # allows to unlock resource on password reset
+        def unlockable?(resource)
+          resource.respond_to?(:unlock_access!) &&
+            resource.respond_to?(:unlock_strategy_enabled?) &&
+            resource.unlock_strategy_enabled?(:email)
+        end
 end
