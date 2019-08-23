bb2404f4697810db477032401d1cddbde8b1d7e9
Jamie <tino7060@likelion.org>
2019-08-23 15:57:06 +0900

    fix analysis

--- app/controllers/user/analysis_controller.rb
+++ app/controllers/user/analysis_controller.rb
@@ -5,7 +5,6 @@ class User::AnalysisController < ApplicationController
   def get
     require 'json'
     require 'http'
-    require 'fuzzystringmatch'
 
     response = HTTP.get("https://www.hira.or.kr/rg/dur/getRestListJson.do?medcCd=#{@codes}")
     rest = JSON.parse(response)["data"]["rest"]
@@ -20,13 +19,7 @@ class User::AnalysisController < ApplicationController
         dur["description"] = yak["durSdEft"]
         put << dur
       }
-      #유사도 판정
-      jarow = FuzzyStringMatch::JaroWinkler.create( :pure )
-      put.combination(2).any? { |pair|
-        if jarow.getDistance( pair.first.to_s, pair.second.to_s) > 0.9
-          put = put - [pair.second]
-        end
-      }
+    put.uniq!
       @result["interactions"] = put
     end
     
@@ -82,13 +75,7 @@ class User::AnalysisController < ApplicationController
         dur["description"] = "약의 효능효과·성분이 동일한 약물이 2가지 이상 있는 경우로 결과는 단순 참고용입니다"
         put << dur
       }
-      #유사도 판정
-      jarow = FuzzyStringMatch::JaroWinkler.create( :pure )
-      put.combination(2).any? { |pair|
-        if jarow.getDistance( pair.first.to_s, pair.second.to_s) > 0.9
-          put = put - [pair.second]
-        end
-      }
+      put.uniq!
       @result["same_ingr"] = put
     end
 
@@ -102,13 +89,7 @@ class User::AnalysisController < ApplicationController
         dur["description"] = "약의 성분은 다르나 효능이 동일한 약물이 2가지 이상 있는 경우"
         put << dur
       }
-      #유사도 판정
-      jarow = FuzzyStringMatch::JaroWinkler.create( :pure )
-      put.combination(2).any? { |pair|
-        if jarow.getDistance( pair.first.to_s, pair.second.to_s) > 0.9
-          put = put - [pair.second]
-        end
-      }
+      put.uniq!
       @result["duplicate"] = put
     end
     #용량주의
@@ -162,7 +143,6 @@ class User::AnalysisController < ApplicationController
   def get_by_drug
     require 'json'
     require 'http'
-    require 'fuzzystringmatch'
 
     response = HTTP.get("https://www.hira.or.kr/rg/dur/getRestListJson.do?medcCd=#{@codes}")
     rest = JSON.parse(response)["data"]["rest"]
@@ -177,13 +157,7 @@ class User::AnalysisController < ApplicationController
         dur["description"] = yak["durSdEft"]
         put << dur
       }
-      #유사도 판정
-      jarow = FuzzyStringMatch::JaroWinkler.create( :pure )
-      put.combination(2).any? { |pair|
-        if jarow.getDistance( pair.first.to_s, pair.second.to_s) > 0.9
-          put = put - [pair.second]
-        end
-      }
+      put.uniq!
       @result["interactions"] = put
     end
     
@@ -239,13 +213,7 @@ class User::AnalysisController < ApplicationController
         dur["description"] = "약의 효능효과·성분이 동일한 약물이 2가지 이상 있는 경우로 결과는 단순 참고용입니다"
         put << dur
       }
-      #유사도 판정
-      jarow = FuzzyStringMatch::JaroWinkler.create( :pure )
-      put.combination(2).any? { |pair|
-        if jarow.getDistance( pair.first.to_s, pair.second.to_s) > 0.9
-          put = put - [pair.second]
-        end
-      }
+      put.uniq!
       @result["same_ingr"] = put
     end
 
@@ -259,13 +227,145 @@ class User::AnalysisController < ApplicationController
         dur["description"] = "약의 성분은 다르나 효능이 동일한 약물이 2가지 이상 있는 경우"
         put << dur
       }
-      #유사도 판정
-      jarow = FuzzyStringMatch::JaroWinkler.create( :pure )
-      put.combination(2).any? { |pair|
-        if jarow.getDistance( pair.first.to_s, pair.second.to_s) > 0.9
-          put = put - [pair.second]
-        end
+      put.uniq!
+      @result["duplicate"] = put
+    end
+    #용량주의
+    parent = rest["I"]
+    if !parent.nil?
+      put = []
+      parent.each { |yak|
+        dur = Hash.new
+        dur["name"] = yak["artcnm"]
+        dur["description"] = "이 약은 1일 최대 #{yak["suspDt"]} 이내로 복용해야 하는 용량주의 의약품입니다."
+        put << dur
       }
+      put.uniq!
+      @result["dosage"] = put
+    end
+
+    #투여기간주의
+    parent = rest["J"]
+    if !parent.nil?
+      put = []
+      parent.each { |yak|
+        dur = Hash.new
+        dur["name"] = yak["artcnm"]
+        dur["description"] = "#{yak["suspDt"]} 일을 초과하여 복용하면 부작용이 우려되는 투여기간주의 의약품입니다."
+        put << dur
+      }
+      put.uniq!
+      @result["period"] = put
+    end
+
+    #노인주의
+    parent = rest["L"]
+    if !parent.nil?
+      put = []
+      parent.each { |yak|
+        dur = Hash.new
+        dur["name"] = yak["artcnm"]
+        dur["description"] = "이 약은 #{yak["suspDt"]} 세 이상 고령자가 복용 시 주의해야하는 노인주의 의약품입니다."
+        put << dur
+      }
+      put.uniq!
+      @result["elder"] = put
+    end
+
+
+    @result["Excluded"] = @excluded
+    render json: @result
+
+  end
+
+  def get_by_drug
+    require 'json'
+    require 'http'
+
+    response = HTTP.get("https://www.hira.or.kr/rg/dur/getRestListJson.do?medcCd=#{@codes}")
+    rest = JSON.parse(response)["data"]["rest"]
+    @result = Hash.new
+    #병용금기
+    parent = rest["A"]
+    if !parent.nil?
+      put = []
+      parent.each { |yak|
+        dur = Hash.new
+        dur["name"] = "#{yak["durNmA"]} + #{yak["durNmB"]}"
+        dur["description"] = yak["durSdEft"]
+        put << dur
+      }
+      put.uniq!
+      @result["interactions"] = put
+    end
+    
+    #연령금기
+    parent = rest["B"]
+    if !parent.nil?
+      put = []
+      parent.each { |yak|
+        dur = Hash.new
+        dur["name"] = yak["artcnm"]
+        dur["description"] = "#{yak["spcAge"]} #{yak["spcAgeUnit"]}"
+        put << dur
+      }
+      put.uniq!
+      @result["age"] = put
+    end
+
+    #임부금기
+    parent = rest["C"]
+    if !parent.nil?
+      put = []
+      parent.each { |yak|
+        dur = Hash.new
+        dur["name"] = yak["artcnm"]
+        dur["description"] = yak["imcompReason"]
+        put << dur
+      }
+      put.uniq!
+      @result["pregnancy"] = put
+    end
+
+    #사용(급여)중지
+    parent = rest["D"]
+    if !parent.nil?
+      put = []
+      parent.each { |yak|
+        dur = Hash.new
+        dur["name"] = yak["artcnm"]
+        dur["description"] = yak["suspDt"]
+        put << dur
+      }
+      put.uniq!
+      @result["stop_usage"] = put
+    end
+
+    #동일성분중복
+    parent = rest["G"]
+    if !parent.nil?
+      put = []
+      parent.each { |yak|
+        dur = Hash.new
+        dur["name"] = "#{yak["durNmA"]} 와(과) #{yak["durNmB"]}"
+        dur["description"] = "약의 효능효과·성분이 동일한 약물이 2가지 이상 있는 경우로 결과는 단순 참고용입니다"
+        put << dur
+      }
+      put.uniq!
+      @result["same_ingr"] = put
+    end
+
+    #효능군중복
+    parent = rest["F"]
+    if !parent.nil?
+      put = []
+      parent.each { |yak|
+        dur = Hash.new
+        dur["name"] = "#{yak["durNmA"]} 와(과) #{yak["durNmB"]}"
+        dur["description"] = "약의 성분은 다르나 효능이 동일한 약물이 2가지 이상 있는 경우"
+        put << dur
+      }
+      put.uniq!
       @result["duplicate"] = put
     end
     #용량주의
@@ -637,7 +737,9 @@ class User::AnalysisController < ApplicationController
         end
       #hira_med_code 있는 경우
       else
-        puts select_code.to_s.concat(";")
+        if select_code.to_s.length == 8
+          select_code = "0".concat(select_code.to_s)
+        end
         @codes << select_code.to_s.concat(";")
       end
 
@@ -684,6 +786,9 @@ class User::AnalysisController < ApplicationController
           end
         #hira_med_code 있는 경우
         else
+          if select_code.to_s.length == 8
+            select_code = "0".concat(select_code.to_s)
+          end
           @codes << select_code.to_s.concat(";")
         end
 
