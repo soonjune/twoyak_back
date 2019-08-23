b33515b856a73aa00160ea2374ccfe8dfbf685b0
Jamie <tino7060@likelion.org>
2019-08-23 20:56:02 +0900

    c and p drug serializer disease serializer only one

--- app/serializers/current_drug_serializer.rb
+++ app/serializers/current_drug_serializer.rb
@@ -5,7 +5,7 @@ class CurrentDrugSerializer
     DrugSerializer.new(Drug.find(current_drug.current_drug_id))
   end
   attribute :diseases do |current_drug|
-    DiseaseSerializer.new(current_drug.diseases.limit(1))
+    DiseaseSerializer.new(current_drug.diseases.limit(1)).serializable_hash[:data].first
   end
   attribute :my_review do |current_drug, params|
     DrugReviewSerializer.new(DrugReview.where(user_id: params[:current_user].id, drug_id: current_drug.current_drug_id), {params: {liked_drug_reviews: params[:current_user].l_drug_review_ids}})
