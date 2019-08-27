9452cb2c7f5eeb2b960a20a4151750a8ea237e05
Jamie <tino7060@likelion.org>
2019-08-27 15:52:43 +0900

    fix elasticsearch url for connecting to ec2

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,4 @@
-url = "https://vpc-twoyak-tgdpdr7k4moi4flysnhc4lsosu.ap-northeast-2.es.amazonaws.com"
+url = "http://ec2-54-180-183-186.ap-northeast-2.compute.amazonaws.com"
     Elasticsearch::Model.client = Elasticsearch::Client.new url: url
   Searchkick.client = Elasticsearch::Client.new(hosts: url, retry_on_failure: true, transport_options: {request: {timeout: 250}})
 
