cdaabcf33ca46c15e07c8c480964b98594d93f7b
Jamie <tino7060@likelion.org>
2019-08-28 11:58:21 +0900

    fix synonyms

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -35,7 +35,8 @@ class Drug < ApplicationRecord
   has_many :interactions, through: :drug_ingrs
   has_and_belongs_to_many :dur_ingrs, join_table: "drug_associations", foreign_key: "drug_id", association_foreign_key: "dur_ingr_id"
 
-  searchkick language: "korean", word_start: [:name], word_middle: [:name, :ingr_kor_name, :ingr_eng_name], word: [:name], synonyms: [["aspirin", "acetylsalicylic acid"],["paracetamol", "acetaminophen"], ["viagra", "sildenafil"], ["cialis", "tadalafil"], ["valproic acid", "divalproex"]]
+  searchkick language: "korean", word_start: [:name], word_middle: [:name, :ingr_kor_name, :ingr_eng_name], word: [:name]
+  searchkick synonyms: [["aspirin", "acetylsalicylic acid"],["paracetamol", "acetaminophen", "tylenol"], ["viagra", "sildenafil"], ["cialis", "tadalafil"], ["valproic acid", "divalproex"]]
 
   def search_data
       {
