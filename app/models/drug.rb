e49e855be38e8c2c2e3a63aae56eab7bbb2f1dee
Jamie <tino7060@likelion.org>
2019-08-28 13:11:09 +0900

    fix error

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -35,14 +35,6 @@ class Drug < ApplicationRecord
   has_many :interactions, through: :drug_ingrs
   has_and_belongs_to_many :dur_ingrs, join_table: "drug_associations", foreign_key: "drug_id", association_foreign_key: "dur_ingr_id"
 
-  def synonyms
-    [`"aspirin", "acetylsalicylic acid",
-      "paracetamol", "acetaminophen",
-      "viagra", "sildenafil",
-      "cialis", "tadalafil",
-      "valproic acid", "divalproex"`]
-  end
-
   searchkick language: "korean", word_start: [:name], word_middle: [:name, :ingr_kor_name, :ingr_eng_name], word: [:name]
 
   def search_data
