59fc8c03250784b10eb375cad73787c98e8acb19
Jamie <tino7060@likelion.org>
2019-08-30 14:10:57 +0900

    add missing attributes in supplement

--- app/serializers/supplement_serializer.rb
+++ app/serializers/supplement_serializer.rb
@@ -1,6 +1,6 @@
 class SupplementSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :shopping_site, :name, :product_url, :photo_url, :rating, :shoppingmall_reviews
+  attributes :shopping_site, :name, :enterprise_name, :price, :product_url, :photo_url, :rating, :shoppingmall_reviews
   attribute :rankings do |sup|
     SupplementIngrsSupplementSerializer.new(sup.rankings).serialized_json
   end
