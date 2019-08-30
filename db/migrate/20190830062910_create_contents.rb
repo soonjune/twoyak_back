9150bc9e6483dc93bad833e57bae75deeabec4f7
Jamie <tino7060@likelion.org>
2019-08-30 16:13:06 +0900

    adding thumbnail url

--- db/migrate/20190830062910_create_contents.rb
+++ db/migrate/20190830062910_create_contents.rb
@@ -2,6 +2,7 @@ class CreateContents < ActiveRecord::Migration[5.2]
   def change
     create_table :contents do |t|
       t.string :title
+      t.string :thumbnail_url
       t.text :body
 
       t.timestamps
