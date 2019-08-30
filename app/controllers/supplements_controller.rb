5290e601e9af82065226a26c46b0d123214f4590
Jamie <tino7060@likelion.org>
2019-08-30 14:27:58 +0900

    supplements controller fixed

--- app/controllers/supplements_controller.rb
+++ app/controllers/supplements_controller.rb
@@ -3,8 +3,10 @@ class SupplementsController < ApplicationController
 
   # GET /supplements
   def index
-    @supplements = SupplementSerializer.new(SupplementIngr.find(params[:supplement_ingr_id]).supplements.paginate(page: params[:page], per_page: 12)).serialized_json
-
+    supplements_found = SupplementIngr.find(params[:supplement_ingr_id]).supplements.where(shopping_site: params[:shopping_site])
+    @supplements = SupplementSerializer.new(supplements_found.paginate(page: params[:page], per_page: 12)).serialized_json
+    #헤더에 total 갯수 넣어줌
+    response.set_header('Total-Count', supplements_found.size)
     render json: @supplements
   end
 
