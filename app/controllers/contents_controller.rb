8291d46861b6a0797bbf9f079429e9c8be70455b
Jamie <tino7060@likelion.org>
2019-09-04 20:09:25 +0900

    fix content controller

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -14,6 +14,7 @@ class ContentsController < ApplicationController
   # GET /contents/1
   def show
     @content.thumbnail_url = url_for(@content.thumbnail_image)
+    @content.save
     render json: @content
   end
 
