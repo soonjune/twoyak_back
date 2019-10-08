71e554daa1bbd33df58fe60cd1929bddbae5fec7
Jamie <tino7060@likelion.org>
2019-10-08 18:03:04 +0900

    fix s3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -57,14 +57,6 @@ class ApplicationController < ActionController::API
     end
   end
 
-  #S3 bucket selection
-  def s3_service
-    service = ActiveStorage::Blob.service
-    return unless service.class.to_s ==   'ActiveStorage::Service::S3Service'
-    service
-  end
-  
-
   # #Override Devise's authenticate_user! method
   # def authenticate_user!(options = {})
   #   head :unauthorized unless signed_in?
--- config/initializers/multi_bucket_active_storage.rb
+++
@@ -1,7 +0,0 @@
-module Methods
-    def set_bucket(bucket)
-      @bucket = bucket
-    end
-  end
-  ActiveStorage::Service.module_eval { attr_writer :bucket }
-  ActiveStorage::Service.class_eval { include Methods }
