6a2e4fb68402f75d8e34f97fcbae729554d15616
Jamie <tino7060@likelion.org>
2019-10-08 14:41:18 +0900

    Revert "fix aws connection"
    This reverts commit 254aa024b8fec4e776d298cf1a34590e414ba74f.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -56,6 +56,13 @@ class ApplicationController < ActionController::API
       return false
     end
   end
+
+  #S3 bucket selection
+  def s3_service
+    service = ActiveStorage::Blob.service
+    return unless service.class.to_s ==   'ActiveStorage::Service::S3Service'
+    service
+  end
   
 
   # #Override Devise's authenticate_user! method
