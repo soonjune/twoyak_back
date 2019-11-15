90895d006868ce4a18b5d8e78adbf2cac46614f6
Jamie <tino7060@likelion.org>
2019-11-15 15:54:59 +0900

    add another in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'mvponstreet.s3-website.ap-northeast-2.amazonaws.com',
             'https://twoyak.com',
             'api.twoyak.com',
-            'localhost:21865'
+            'localhost:21865',
+            'localhost:21866'
 
     resource '*',
       headers: :any,
