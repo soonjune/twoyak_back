bfc6965e80386b80c7e8ac229d19cb9d3c492fa7
Jamie <tino7060@likelion.org>
2019-11-21 14:26:56 +0900

    version update to 1.0.4

--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -1,7 +1,7 @@
 class MobileController < ApplicationController
     def version
         @result = Hash.new
-        @result['data'] = '1.0.3'
+        @result['data'] = '1.0.4'
         render json: @result
     end
 end
